85552a52dbca2342b33ddb9f465b86228a7681bc
DESKTOP-IT23LMU\3bdulrahman <cabdulraxmann@gmail.com>
2017-02-15 16:28:05 +0300

    commit.

--- SourceControlTestApp/Controllers/HomeController.cs
+++ SourceControlTestApp/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace SourceControlTestApp.Controllers
             ViewBag.Message = "Your application description page.";
 
             return View();
-        }
+        }fffff
 
         public ActionResult Contact()
         {
